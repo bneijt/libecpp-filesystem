95ef5857d1de9b9904e26ce64dcd21e6658b5d62
Marten van de Sanden <mart.vd.sanden@gmail.com>
2009-10-29 20:31:50 +0100

    - added check to nog up into an unexisting dir

--- src/path/up.cc
+++ src/path/up.cc
@@ -4,7 +4,8 @@ Path Path::up() const
 {
   vector<string> split = String::split(d_path,"/");
 
-  split.pop_back();
+  if (!split.empty())
+    split.pop_back();
 
   return Path(String::combine(split,"/"));
 }
